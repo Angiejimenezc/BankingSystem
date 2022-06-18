0909f486ea3502f4360766bfe19ea09a8e6d5e57
Angie <angiejimenez@gmail.com>
2022-06-18 21:28:21 +0200

    Added more methods

--- src/main/java/com/project/BankingSystem/controller/impl/BankingSystemController.java
+++ src/main/java/com/project/BankingSystem/controller/impl/BankingSystemController.java
@@ -42,49 +42,67 @@ public class BankingSystemController implements IBankingSystemController {
         return bankingSystemService.findAccountHolder(id);
     }
 
-    @Override
-    public Money getAccountBalance(Integer secretKey) {
-        return null;
+    //  Getter of the balance of any account using secret keys
+    @GetMapping("/get/account-balance/{secretKey}")
+    @ResponseStatus(HttpStatus.OK)
+    public Money getAccountBalance(@PathVariable Integer secretKey) {
+        return bankingSystemService.getAccountBalance(secretKey);
     }
 
-    @Override
-    public Money getCreditCardBalance(Long id) {
-        return null;
+    //  Gets credit card balance using the id
+    @GetMapping("/get/credit-card-balance/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Money getCreditCardBalance(@PathVariable @Valid Long id) {
+        return bankingSystemService.getCreditCardBalance(id);
     }
 
-    @Override
-    public void transferMoney(Long id, Long targetId, QuantityDTO quantityDTO) {
-
+    //  Account holders will be able to transfer any amount(if sufficient funds) to any account
+    @PatchMapping("/transfer/{id}/{targetId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void transferMoney(@PathVariable @Valid Long id, @PathVariable @Valid Long targetId,
+                              @RequestBody @Valid QuantityDTO quantityDTO) {
+        bankingSystemService.transferMoney(id, targetId, quantityDTO.getQuantity());
     }
 
-    @Override
+    //  Admins can see all the accounts in the DB with this method.
+    @GetMapping("/show-accounts")
     public List<Account> accounts() {
-        return null;
+        List<Account> accounts = bankingSystemService.showAccounts();
+        return accounts;
     }
 
-    @Override
-    public void creditAccountBalance(Long id, QuantityDTO quantity) {
-
+    //  Admins can credit or debit an account with the following two methods
+    @PatchMapping("/credit/account/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void creditAccountBalance(@PathVariable @Valid Long id, @RequestBody @Valid QuantityDTO quantity) {
+        bankingSystemService.creditAccountBalance(id, quantity.getQuantity());
     }
 
-    @Override
-    public void debitAccountBalance(Long id, QuantityDTO quantity) {
-
+    @PatchMapping("/debit/account/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void debitAccountBalance(@PathVariable @Valid Long id, @RequestBody @Valid QuantityDTO quantity) {
+        bankingSystemService.debitAccountBalance(id, quantity.getQuantity());
     }
 
-    @Override
-    public void creditAccountTP(Integer hashedKey, TransferThirdPartyDTO transferThirdPartyDTO) {
-
+    //  Third parties can debit or credit an account with the following two methods. They have to provide their hashed key
+//  in the request.
+    @PatchMapping("/creditTP/account/{hashedKey}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void creditAccountTP(@PathVariable Integer hashedKey, @RequestBody @Valid TransferThirdPartyDTO transferThirdPartyDTO) {
+        bankingSystemService.creditAccountTP(transferThirdPartyDTO.getAmount(), transferThirdPartyDTO.getId());
+    }
+    @PatchMapping("/debitTP/account/{hashedKey}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void debitAccountTP(@PathVariable Integer hashedKey, @RequestBody @Valid TransferThirdPartyDTO transferThirdPartyDTO) {
+        bankingSystemService.debitAccountTP(transferThirdPartyDTO.getAmount(), transferThirdPartyDTO.getId());
     }
 
-    @Override
-    public void debitAccountTP(Integer hashedKey, TransferThirdPartyDTO transferThirdPartyDTO) {
-
-    }
-
-    @Override
-    public Object createChecking(CheckingDTO checkingDTO) {
-        return null;
+    //  Admins can create a checking account with this method. It will create a student checking account if the account
+//  holder is 24 years old or less.
+    @PostMapping("/create/checking")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Object createChecking(@RequestBody @Valid CheckingDTO checkingDTO) {
+        return bankingSystemService.validateAgeToCreateAccount(checkingDTO.getQuantity(), checkingDTO.getId());
     }
 
     //  Allows admins to add a new account holder to the DB
@@ -94,8 +112,11 @@ public class BankingSystemController implements IBankingSystemController {
         return bankingSystemService.createAccountHolder(accountHolderDTO);
     }
 
-    @Override
-    public ThirdParty createThirdParty(ThirdPartyDTO thirdPartyDTO) {
-        return null;
+    //  Admins must provide a name and a password to create the third party user. The username will be the third party's
+//  name to lower case + "_tp", the password will be automatically encoded, and the hashed key will be set randomly.
+    @PostMapping("/create/third-party")
+    @ResponseStatus(HttpStatus.CREATED)
+    public ThirdParty createThirdParty(@RequestBody @Valid ThirdPartyDTO thirdPartyDTO) {
+        return bankingSystemService.createThirdParty(thirdPartyDTO.getName(), thirdPartyDTO.getPassword());
     }
 }
--- src/main/java/com/project/BankingSystem/models/Transaction.java
+++ src/main/java/com/project/BankingSystem/models/Transaction.java
@@ -1,4 +1,7 @@
 package com.project.BankingSystem.models;
 
 public class Transaction {
+
+
+
 }
